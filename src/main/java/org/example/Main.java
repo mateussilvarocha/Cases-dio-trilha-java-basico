a134736c72dc2814d52f469f0fc28ee6f391a628
Mateus Silva Rocha <mateus_msr1@yahoo.com>
2025-08-05 20:35:17 -0300

    resolução do case 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,45 @@ package org.example;
 
 import java.util.Scanner;
 
+import static java.text.ChoiceFormat.nextDouble;
+import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static void main(String[] args) {
+
+        //        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Digite o salários pretendido por você:");
+//        String salario_pretendido = scanner.next();
+//        analisar_candidato("1200");
+        selecaoCandidato();
+    }
+    static void selecaoCandidato(){
+        String[] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto","Felipe", "Marcia", "Julia", "Paulo", "Augusto"};
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
         double salario_base$ = 2000.00;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o salários pretendido por você:");
-        String salario_pretendido = scanner.next();
-        double salario_pretendido$ = Double.parseDouble(salario_pretendido);
+
+        //array ultrapssaou a quantidade de itens para verificação
+        //precisa de um 'breackpoint' mais elaborado que: while(candidatosSelecionados < 5){}
+        // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
+        //	at org.example.Main.selecaoCandidato(Main.java:23)
+        //	at org.example.Main.main(Main.java:14)
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+
+            String candidato = candidatos[candidatosAtual];
+
+            double salarioPretendido = valorPrentendido();
+
+            System.out.println("O candidato "+ candidato+" Solicitou este valor de salário "+ salarioPretendido);
+            if(salario_base$ >= salarioPretendido){
+                System.out.println("O candidato "+ candidato+" foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static void analisar_candidato(double salario_pretendido$ ){
+        double salario_base$ = 2000.00;
+//        double salario_pretendido$ = Double.parseDouble(string_salario_pretendido);
 
         if(salario_base$ > salario_pretendido$){
             System.out.println("LIGAR PARA O CANDIDATO");
@@ -19,6 +51,11 @@ public class Main {
             System.out.println("AGUARDANDO RESULTADO DEMAIS CANDIDATOS");
         }
 
+    }
+
+
+    static double valorPrentendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
 
 
